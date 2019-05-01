c759f2d34c0839e40c23b3fa35e1933fb82cbd3f
Alex Barbosa <abarbosasilva7@gmail.com>
2019-05-01 17:51:03 -0300

    Implementado a recuperação de senha no backend

--- src/app/components/security/login/login.component.ts
+++ src/app/components/security/login/login.component.ts
@@ -1,5 +1,5 @@
-import { StorageService } from './../../../../services/storage.service';
-import { LocalUser } from './../../../../models/local-user';
+import { MessageService } from 'primeng/api';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CredenciaisDTO } from './../../../../models/credencias.dto';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../../services/auth.service';
@@ -19,15 +19,21 @@ export class LoginComponent implements OnInit {
     email: '',
     senha: ''
   };
-
+  formSenha: FormGroup;
   shared: SharedService;
 
   constructor(
     private auth: AuthService,
     private router: Router,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService
   ) {
     this.shared = SharedService.getInstance();
+
+    this.formSenha = this.formBuilder.group({
+      email: ['',[Validators.required, Validators.email]]
+    });
   }
 
   ngOnInit() { }
@@ -52,6 +58,29 @@ export class LoginComponent implements OnInit {
       this.spinner.hide();
     }, 2000);
 
+  }
+
+  forgot() {
+    console.log(this.formSenha.value)
+    this.auth.forgot(this.formSenha.value).subscribe(
+      response =>{
+        this.showInsertOk();
+      },
+      error => {}
+    )
+  }
+
+  showInsertOk() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'E-mail solicitado com sucesso !!!',
+      detail: ''
+    });
+
+    this.formSenha = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]]
+
+    });
   }
 
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,7 +5,7 @@ import { CredenciaisDTO } from './../models/credencias.dto';
 import { Injectable } from '@angular/core';
 import { LocalUser } from '../models/local-user';
 import { StorageService } from './storage.service';
-import { JwtHelperService} from '@auth0/angular-jwt';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthService {
@@ -16,15 +16,15 @@ export class AuthService {
         public http: HttpClient,
         public storage: StorageService,
         private cartService: CartService
-        ) {}
+    ) { }
 
     authenticate(creds: CredenciaisDTO) {
 
-       return this.http.post(`${API_CONFIG.baseURL}/login`,
-        creds, {
-            observe: 'response',
-            responseType: 'text'
-        });
+        return this.http.post(`${API_CONFIG.baseURL}/login`,
+            creds, {
+                observe: 'response',
+                responseType: 'text'
+            });
     }
 
     refreshToken() {
@@ -35,21 +35,31 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });
-}
+    }
+
+    forgot(email: string) {
+        return this.http.post(
+            `${API_CONFIG.baseURL}/auth/forgot`,
+            email,
+            {
+                observe: 'response',
+                responseType: 'text'
+            });
+    }
 
     successfullLogin(authorizationValue: string) {
 
         const tok = authorizationValue.substring(7);
         const user: LocalUser = {
-          token: tok,
-          email: this.jwtHelper.decodeToken(tok).sub
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
         this.cartService.createOrClearCart();
-      }
+    }
 
-      logout() {
+    logout() {
         this.storage.setLocalUser(null);
-      }
+    }
 
 }
